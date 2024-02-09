e0a2ba2e5ed05b45ded3e1fea52942a0ce7c3dcf
Vlad Rogozin <vroghozin@gmail.com>
2024-02-09 17:46:36 +0200

    add test playr

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,7 +99,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
